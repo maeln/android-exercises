8c10f4fb36a73c82b85d194b2ee2a1f31a0d7ee5
maeln <contact@maeln.com>
2017-02-20 15:25:44 +0100

    fix

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -20,14 +23,22 @@ public class LibraryActivity extends AppCompatActivity {
 
         Button openButton = (Button) findViewById(R.id.openButton);
 
-        Book book = new Book("Garry Whopper", "CK Rowling");
+        final Book book = new Book("Garry Whopper", "CK Rowling");
 
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
                 Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
-                // Add book to intent
+                intent.putExtra("BOOK", book);
                 startActivity(intent);
+                */
+                new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int y, int m, int d) {
+                        Toast.makeText(LibraryActivity.this, d+"/"+(m+1)+"/"+y, Toast.LENGTH_LONG).show();
+                    }
+                }, 2017, 02, 25).show();
             }
         });
     }
