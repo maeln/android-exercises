7b258fa20c504497335a9f8340373e0ddc3f3221
maeln <contact@maeln.com>
2017-02-20 16:54:06 +0100

    lol

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -12,6 +15,8 @@ public class LibraryActivity extends AppCompatActivity {
 
         // TODO Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        ImageView imgView = (ImageView) findViewById(R.id.downloadImageView);
+        Glide.with(this).load("https://chibr.eu/itii_pdl.jpg").into(imgView);
     }
 
 }
